a2a1d234e2af8968cf3c0298d23a572b11a73bd1
Gerd Hoffmann <kraxel@redhat.com>
2022-03-16 13:10:42 +0100

    add json unit test

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,30 @@
 import os
+import json
 import unittest
 
 from ovmfctl.efi import edk2
+from ovmfctl.efi import efijson
 
-EMPTY_VARS = "/usr/share/OVMF/OVMF_VARS.fd"
+VARS_EMPTY   = "/usr/share/OVMF/OVMF_VARS.fd"
+VARS_SECBOOT = "/usr/share/OVMF/OVMF_VARS.secboot.fd"
 
-@unittest.skipUnless(os.path.exists(EMPTY_VARS), 'no VARS file')
 class TestsEdk2(unittest.TestCase):
 
+    @unittest.skipUnless(os.path.exists(VARS_EMPTY), 'no empty vars file')
     def test_enroll(self):
-        store = edk2.Edk2VarStore(EMPTY_VARS)
+        store = edk2.Edk2VarStore(VARS_EMPTY)
         varlist = store.get_varlist()
         varlist.enroll_platform_redhat()
         varlist.add_microsoft_keys()
         varlist.enable_secureboot()
         blob = store.bytes_varstore(varlist)
+
+    @unittest.skipUnless(os.path.exists(VARS_EMPTY), 'no secboot vars file')
+    def test_json(self):
+        store = edk2.Edk2VarStore(VARS_SECBOOT)
+        varlist = store.get_varlist()
+        j = json.dumps(varlist, cls=efijson.EfiJSONEncoder, indent = 4)
+        l = json.loads(j, object_hook = efijson.efi_decode)
 
 if __name__ == '__main__':
     unittest.main()
