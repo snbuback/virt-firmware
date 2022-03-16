f60f21eb563172917b5626bdca29939bcb93396d
Gerd Hoffmann <kraxel@redhat.com>
2022-03-16 07:25:22 +0100

    add json decoder

--- ovmfctl/efi/efijson.py
+++ ovmfctl/efi/efijson.py
@@ -2,6 +2,8 @@
 """ json support for efi """
 import json
 
+from ovmfctl.efi import guids
+from ovmfctl.efi import ucs16
 from ovmfctl.efi import efivar
 
 # pylint: disable=no-self-use
@@ -35,3 +37,21 @@ class EfiJSONEncoder(json.JSONEncoder):
         if isinstance(o, efivar.EfiVarList):
             return self.efivarlist(o)
         return json.JSONEncoder.default(self, o)
+
+def efi_decode(obj):
+    if 'guid' in obj:
+        var = efivar.EfiVar(ucs16.from_string(obj.get('name')),
+                            guid = guids.parse_str(obj.get('guid')),
+                            attr = obj.get('attr'),
+                            data = bytes.fromhex(obj.get('data')))
+        if obj.get('time'):
+            var.parse_time(bytes.fromhex(obj.get('time')), 0)
+        return var
+
+    if 'variables' in obj:
+        varlist = efivar.EfiVarList()
+        for var in obj.get('variables'):
+            varlist[str(var.name)] = var
+        return varlist
+
+    return obj
