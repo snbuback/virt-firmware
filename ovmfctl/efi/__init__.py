c83cb46441fda7ad6fc933fb1df1e6f93252a7e3
Gerd Hoffmann <kraxel@redhat.com>
2022-03-23 13:15:51 +0100

    add linux

--- ovmfctl/efi/__init__.py
+++ ovmfctl/efi/__init__.py
@@ -1,2 +1,2 @@
 """ efi helper modules """
-__all__ = [ "guids", "ucs16", "devpath", "siglist", "efivar", "efijson", "edk2", "certs" ]
+__all__ = [ "guids", "ucs16", "devpath", "siglist", "efivar", "efijson", "edk2", "certs", "linux" ]
