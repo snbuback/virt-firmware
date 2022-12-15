f3406a6ad250161ba1f5f20b4a35ba8826b8aef3
Gerd Hoffmann <kraxel@redhat.com>
2022-12-15 07:45:10 +0100

    multibank, output tweaks

--- experimental/measure.py
+++ experimental/measure.py
@@ -19,12 +19,14 @@ from virt.firmware.varstore import edk2
 # pcr calculation
 
 class PCR:
-    """ tpm pcr register """
+    """ tpm2 pcr register """
 
     def __init__(self):
-        self.count = 0
         self.banks = {
-            'sha256' : b'\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'
+            'sha1'   : bytes(160 >> 3),
+            'sha256' : bytes(256 >> 3),
+            'sha384' : bytes(384 >> 3),
+            'sha512' : bytes(512 >> 3),
         }
 
     def extend_hash(self, bank, hashdata):
@@ -32,33 +34,51 @@ class PCR:
         h.update(self.banks[bank])
         h.update(hashdata)
         self.banks[bank] = h.digest()
-        self.count += 1
 
     def value(self, bank):
         return self.banks[bank]
 
-def calculate_pcr(index, bank, result):
-    pcr = PCR()
+def calculate_pcrs(banks, result):
+    pcrs = {}
     for item in result:
-        if item['PCRIndex'] != index:
-            continue
-        pcr.extend_hash(bank, bytes.fromhex(item['Digests'][bank]))
+        index = item['PCRIndex']
+        pcr = pcrs.get(index)
+        if not pcr:
+            pcr = PCR()
+            pcrs[index] = pcr
+        for bank in banks:
+            pcr.extend_hash(bank, bytes.fromhex(item['Digests'][bank]))
 
-    if pcr.count:
-        print(f'# pcr {index}, bank {bank}: {pcr.value(bank).hex()}')
+    values = []
+    for index in sorted(pcrs.keys()):
+        pcr = pcrs.get(index)
+        item = {
+            'PCRIndex' : index,
+            'Values'   : {},
+        }
+        for bank in banks:
+            item['Values'][bank] = pcr.value(bank).hex()
+        values.append(item)
+    return values
 
 
 ########################################################################
 # measure misc
 
-def measure_sep(index):
-    separator = b'\0\0\0\0'
+def hash_digest(banks, hashdata):
+    result = {}
+    for bank in banks:
+        h = hashlib.new(bank)
+        h.update(hashdata)
+        result[bank] = h.hexdigest()
+    return result
+
+def measure_sep(index, banks):
+    separator = bytes(4)
     result = {
         'PCRIndex'   : index,
         'EventType'  : 'EV_SEPARATOR',
-        'Digests'    : {
-            'sha256' : hashlib.sha256(separator).hexdigest(),
-        },
+        'Digests'    : hash_digest(banks, separator),
     }
     return result
 
@@ -66,7 +86,7 @@ def measure_sep(index):
 ########################################################################
 # measure vars
 
-def measure_var(var):
+def measure_var(banks, var, cfg):
     namelen = len(var.name.data) >> 1
     datalen = len(var.data)
 
@@ -77,14 +97,17 @@ def measure_var(var):
     varlog += var.name.data
     varlog += var.data
 
+    if cfg:
+        evttype = 'EV_EFI_VARIABLE_DRIVER_CONFIG'
+    else:
+        evttype = 'EV_EFI_VARIABLE_AUTHORITY'
+
     # modeled after tpm2_eventlog output
     result = {
-        'PCRIndex'   : 7,
-        'EventType'  : 'EV_EFI_VARIABLE_DRIVER_CONFIG',
-        'Digests'    : {
-            'sha256' : hashlib.sha256(varlog).hexdigest(),
-        },
-        'Event' : {
+        'PCRIndex'  : 7,
+        'EventType' : evttype,
+        'Digests'   : hash_digest(banks, varlog),
+        'Event'     : {
             'VariableName'       : str(var.guid),
             'UnicodeNameLength'  : namelen,
             'VariableDataLength' : datalen,
@@ -93,27 +116,30 @@ def measure_var(var):
     }
     return result
 
-def measure_varlist(varlist, secureboot = True):
+def measure_varlist(banks, varlist,
+                    secureboot = True,
+                    shim = True):
     result = []
 
     sb = efivar.EfiVar(ucs16.from_string('SecureBoot'))
     sb.set_bool(secureboot)
-    result.append(measure_var(sb))
+    result.append(measure_var(banks, sb, True))
 
     for name in ('PK', 'KEK', 'db', 'dbx'):
         var = varlist.get(name)
         if var:
-            result.append(measure_var(var))
+            result.append(measure_var(banks, var, True))
 
-    result.append(measure_sep(7))
+    result.append(measure_sep(7, banks))
 
-    var = varlist.get('SbatLevel')
-    if var:
-        result.append(measure_var(var))
+    if shim:
+        var = varlist.get('SbatLevel')
+        if var:
+            result.append(measure_var(banks, var, False))
 
-    sb = efivar.EfiVar(ucs16.from_string('MokListTrusted'))
-    sb.set_bool(True)
-    result.append(measure_var(sb))
+        sb = efivar.EfiVar(ucs16.from_string('MokListTrusted'))
+        sb.set_bool(True)
+        result.append(measure_var(banks, sb, False))
 
     return result
 
@@ -134,34 +160,32 @@ def find_volume(item, nameguid = None, typeguid = None):
                 return r
     return None
 
-def measure_volume(name, vol):
+def measure_volume(banks, name, vol):
     # modeled after tpm2_eventlog output
     result = {
         'PCRIndex'   : 0,
         'EventType'  : 'EV_EFI_PLATFORM_FIRMWARE_BLOB',
         'VolumeName' : name,
-        'Digests'    : {
-            'sha256' : vol.sha256.hexdigest()
-        },
+        'Digests'    : hash_digest(banks, vol.blob),
         'Event' : {
             'BlobLength': vol.tlen,
         },
     }
     return result
 
-def measure_image(image):
+def measure_image(banks, image):
     result = []
     peifv = find_volume(image, nameguid = guids.OvmfPeiFv)
     dxefv = find_volume(image, nameguid = guids.OvmfDxeFv)
     varfv = find_volume(image, typeguid = guids.NvData)
     if peifv:
-        result.append(measure_volume('PEIFV', peifv))
+        result.append(measure_volume(banks, 'PEIFV', peifv))
     if dxefv:
-        result.append(measure_volume('DXEFV', dxefv))
+        result.append(measure_volume(banks, 'DXEFV', dxefv))
     if varfv:
         edk2store = edk2.Edk2VarStore(image.name)
         varlist = edk2store.get_varlist()
-        result += measure_varlist(varlist)
+        result += measure_varlist(banks, varlist)
     return result
 
 
@@ -170,23 +194,35 @@ def measure_image(image):
 
 def main():
     parser = optparse.OptionParser()
-    parser.add_option('-l', '--loglevel', dest = 'loglevel', type = 'string', default = 'info',
+    parser.add_option('-l', '--loglevel', dest = 'loglevel', type = 'string', default = 'warn',
                       help = 'set loglevel to LEVEL', metavar = 'LEVEL')
     parser.add_option('--image', dest = 'image', type = 'string',
                       help = 'read edk2 image from FILE', metavar = 'FILE')
     parser.add_option('--vars', dest = 'vars', type = 'string',
                       help = 'read edk2 vars from FILE', metavar = 'FILE')
+    parser.add_option('--no-sb', '--no-secure-boot', dest = 'secureboot',
+                      action = 'store_false', default = True)
+    parser.add_option('--no-shim', dest = 'shim',
+                      action = 'store_false', default = True)
+    parser.add_option('--bank', dest = 'banks',
+                      action = 'append', type = 'string')
+    parser.add_option('--eventlog', '--event-log',
+                      dest = 'eventlog', action = 'store_true', default = False)
+    parser.add_option('--pcrs', dest = 'pcrs', action = 'store_true', default = False)
     (options, args) = parser.parse_args()
 
     logging.basicConfig(format = '%(levelname)s: %(message)s',
                         level = getattr(logging, options.loglevel.upper()))
-    result = []
+    eventlog = []
+
+    if not options.banks:
+        options.banks = [ 'sha256', ]
 
     if options.image:
         with open(options.image, 'rb') as f:
             data = f.read()
         image = dump.Edk2Image(options.image, data)
-        result = measure_image(image)
+        eventlog = measure_image(options.banks, image)
 
     elif options.vars:
         if edk2.Edk2VarStore.probe(options.vars):
@@ -198,11 +234,15 @@ def main():
         else:
             logging.error("unknown input file format")
             return 1
-        result = measure_varlist(varlist)
+        eventlog = measure_varlist(options.banks, varlist,
+                                   secureboot = options.secureboot)
 
+    result = []
+    if options.eventlog:
+        result += eventlog
+    if options.pcrs:
+        result += calculate_pcrs(options.banks, eventlog)
     print(json.dumps(result, indent = 4))
-    calculate_pcr(0, 'sha256', result)
-    calculate_pcr(7, 'sha256', result)
 
     return 0
 
