95fe38e64c4ee4b3efffd0aea38d69001cf5a01f
Gerd Hoffmann <kraxel@redhat.com>
2023-04-14 09:26:28 +0200

    run pe tests on shim

--- tests/test-pe.sh
+++ tests/test-pe.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 kernel="/boot/vmlinuz-$(uname -r)"
+shim="$(echo /boot/efi/EFI/*/shim*.efi)"
 
 # run tests
 set -ex
 pe-dumpinfo --help
 pe-listsigs --help
 pe-addsigs --help
-if test -f "${kernel}"; then
-    pe-listsigs "${kernel}"
-fi
+for file in ${kernel} ${shim}; do
+    if test -f "${file}"; then
+        pe-listsigs "${file}"
+    fi
+done
