49917b37e6b00dd167e017ebf5c04a299375215c
Gerd Hoffmann <kraxel@redhat.com>
2023-04-28 13:35:16 +0200

    catch file access errors

--- virt/firmware/varstore/linux.py
+++ virt/firmware/varstore/linux.py
@@ -15,13 +15,16 @@ class LinuxVarStore:
     def get_variable(name, guid,
                      path = '/sys/firmware/efi/efivars'):
         filename = os.path.join(path, f'{name}-{guid}')
-        with open(filename, "rb") as f:
-            attr = int.from_bytes(f.read(4), byteorder='little', signed=False)
-            data = f.read()
-        var = efivar.EfiVar(ucs16.from_string(name),
-                            guid = guids.parse_str(guid),
-                            attr = attr, data = data)
-        return var
+        try:
+            with open(filename, "rb") as f:
+                attr = int.from_bytes(f.read(4), byteorder='little', signed=False)
+                data = f.read()
+                var = efivar.EfiVar(ucs16.from_string(name),
+                                    guid = guids.parse_str(guid),
+                                    attr = attr, data = data)
+            return var
+        except OSError:
+            return None
 
     @staticmethod
     def get_varlist(path = '/sys/firmware/efi/efivars',
@@ -40,6 +43,6 @@ class LinuxVarStore:
                 guid = entry.name[ len(entry.name) - 36 : ]
                 filename = os.path.join(path, entry.name)
                 var = LinuxVarStore.get_variable(name, guid, path)
-                if var.attr & efivar.EFI_VARIABLE_NON_VOLATILE or volatile:
+                if var and (var.attr & efivar.EFI_VARIABLE_NON_VOLATILE or volatile):
                     varlist[name] = var
         return varlist
