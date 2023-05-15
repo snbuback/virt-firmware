8ead86f5e114860e77caf9e5668882f902f89042
Gerd Hoffmann <kraxel@redhat.com>
2023-05-15 15:55:08 +0200

    add experimental/bootmenu.py

---
+++ experimental/bootmenu.py
@@ -0,0 +1,53 @@
+#/usr/bin/python3
+""" efi boot menu """
+import os
+import sys
+import argparse
+import subprocess
+
+from virt.firmware.bootcfg import linuxcfg
+
+def bootmenu(cfg):
+    rows = os.get_terminal_size().lines
+    cols = os.get_terminal_size().columns
+    cmdline = [ 'dialog', '--menu', 'efi boot menu',
+                str(rows - 10), str(cols - 30), str(rows - 18) ]
+
+    for (nr, entry) in cfg.bentr.items():
+        cmdline += [ str(nr), str(entry.title) ]
+
+    result = subprocess.run(cmdline, stderr = subprocess.PIPE, check = True)
+    subprocess.run(['clear',], check = True)
+    nr = result.stderr.decode()
+    if nr == '':
+        return None
+    return int(nr)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = 'efi boot menu')
+    parser.add_argument('--reboot', dest = 'reboot',
+                        default = False, action = 'store_true',
+                        help = 'reboot after picking an entry')
+    options = parser.parse_args()
+
+    cfg = linuxcfg.LinuxEfiBootConfig()
+    nr = bootmenu(cfg)
+    if nr is None:
+        return 0
+
+    cfg.set_boot_next(nr)
+    try:
+        cfg.linux_update_next()
+    except PermissionError:
+        print('Can not update BootNext (try run as root)')
+        return 1
+
+    if options.reboot:
+        message = f'reboot into {cfg.bentr[nr].title}'
+        subprocess.run(['/usr/sbin/shutdown', '-r', 'now', message], check = True)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
